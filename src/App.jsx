21af8b6ab462cc127abcaa3a251e3d385f69f20a
Talha Satti <148781348+talhaStti@users.noreply.github.com>
2024-03-16 20:59:28 +0000

    set homepage for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Talha Satti",
+  title: "Web Designer",
+  email: "talhasatti181@gmail.com",
+  gitHub: "talhaStti",
+  instagram: "_talha_satti",
+  linkedIn: "Talha Satti",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "talha_stti",
+  youTube: "@satti-swag",
 };
 
 const primaryColor = "#4E567E";
